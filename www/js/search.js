6f63f74140f90843c1fee550443203c0ad27e500
me <me@internet.earth>
2020-05-24 21:07:45 -0400

    updates

--- www/js/search.js
+++ www/js/search.js
@@ -7,26 +7,47 @@ var index = elasticlunr(function () {
     this.setRef('i'); //URI
 });
 
-function search(q) {
+
+const _search = q => {
   return index.search(q, {
       fields: {
           n: {boost: 2},
           s: {boost: 1},
       }
-      //,expand: false /*true */
+      ,expand: true /*true */
   });
+};
+const search = memoizerific(256)(_search);
+
+console.log('loading categories...');
+var indexData = window.localStorage['categories'];
+var loaded = indexData!==undefined;
+if (loaded) {
+    //TODO try and if failure remove the cached version and recreate below
+    try {
+      index.load(JSON.parse(indexData));
+      console.log('categories cached.');
+    }catch (e) {
+      console.error(e);
+      loaded = false;
+    }
 }
 
-$.get('cat.json', x=>{
-  //console.log(x);
-  //console.log('categories', x);
-  console.log('loading categories...');
-  var count = 0;
-  for(var i in x) {
-    const item = x[i];
-    item.i = i;
-    index.addDoc(item);
-    count++;
-  }
-  console.log(count, 'categories loaded.');
-});
+if (!loaded) {
+  $.get('cat.json', x=>{
+    //console.log(x);
+    //console.log('categories', x);
+    var count = 0;
+    for(var i in x) {
+      const item = x[i];
+      if (item.s!==undefined) { //OR other filter
+        item.i = i;
+        index.addDoc(item);
+        count++;
+      }
+    }
+    console.log(count, 'categories loaded.');
+    //TODO: https://github.com/adrianmay/rhaboo, or similar
+    //window.localStorage['categories'] = JSON.stringify(index.toJSON(), null, null);
+  });
+}

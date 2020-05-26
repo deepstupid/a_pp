b8ba8244ee97440782bf2a5bd7e64ae66e21a119
me <me@internet.earth>
2020-05-26 14:19:22 -0400

    updates

--- www/js/search.js
+++ www/js/search.js
@@ -1,10 +1,18 @@
 "use strict";
 
+/*var w = new Worker("js/searchworker.js");
+w.onmessage = function(e) {
+	console.log('Message received from worker', e);
+};
+w.postMessage(['s']);*/
+
 var index = elasticlunr(function () {
     this.addField('n'); //label
     //this.addField('S'); //list of supercategories
     this.addField('s'); //list of subcategories
     this.setRef('i'); //URI
+    //this.pipeline._queue.remove()
+    //console.log(this);
 });
 
 
@@ -14,7 +22,7 @@ const _search = q => {
           n: {boost: 2},
           s: {boost: 1},
       }
-      ,expand: true /*true */
+      ,expand: false /*true */
   });
 };
 const search = memoizerific(256)(_search);
@@ -25,7 +33,7 @@ var loaded = indexData!==undefined;
 if (loaded) {
     //TODO try and if failure remove the cached version and recreate below
     try {
-      index.load(JSON.parse(indexData));
+      index.load(JSONC.decompress(indexData));
       console.log('categories cached.');
     }catch (e) {
       console.error(e);
@@ -48,6 +56,12 @@ if (!loaded) {
       }
     }
     console.log(count, 'categories loaded.');
+/*    const y = index.toJSON();
+
+    console.log('compressing');
+    var compressedJSON = JSONC.compress( y );
+    console.log(compressedJSON.length);*/
+
     //TODO: https://github.com/adrianmay/rhaboo, or similar
     //window.localStorage['categories'] = JSON.stringify(index.toJSON(), null, null);
   });
